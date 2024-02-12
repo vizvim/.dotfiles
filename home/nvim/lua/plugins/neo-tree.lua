bf8067a4f61055023359198a11b36adcfa379fe0
Kyle Brantley <me@kylebrantley.dev>
2024-02-11 19:25:58 -0600

    change hiddne files preferences within neotree

--- home/nvim/lua/plugins/neo-tree.lua
+++ home/nvim/lua/plugins/neo-tree.lua
@@ -1,6 +1,13 @@
 return {
 	"nvim-neo-tree/neo-tree.nvim",
 	opts = {
+		filesystem = {
+			filtered_items = {
+				visible = true,
+				hide_dotfiles = false,
+				hide_gitignored = false,
+			},
+		},
 		window = {
 			position = "right",
 		},
