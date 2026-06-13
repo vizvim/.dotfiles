2e4174bb1aaf314a6c5bd651cee5422b23c8a277
Kyle <contact@kyle.zip>
2026-06-12 23:06:06 -0500

    changes

--- nvim/.config/nvim/lua/plugins/snacks.lua
+++ nvim/.config/nvim/lua/plugins/snacks.lua
@@ -1,17 +1,14 @@
 return {
-  "folke/snacks.nvim",
-  opts = {
-    picker = {
-      sources = {
-        explorer = {
-          -- show the file explorer on the right-hand side of the screen
-          layout = { layout = { position = "right" } },
-          -- show dotfiles and gitignored files (parity with the old neo-tree setup);
-          -- toggle at runtime with `H` (hidden) and `I` (ignored)
-          hidden = true,
-          ignored = true,
-        },
-      },
-    },
-  },
+	"folke/snacks.nvim",
+	opts = {
+		picker = {
+			sources = {
+				explorer = {
+					layout = { layout = { position = "right" } },
+					hidden = true,
+					ignored = true,
+				},
+			},
+		},
+	},
 }
